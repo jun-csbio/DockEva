c8d2531b58eb3a28fba6595b5e551869e0172da9
jun-csbio <33274691+jun-csbio@users.noreply.github.com>
2024-10-09 11:12:56 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+g++ -static -O3 -ffast-math -lm -o ./bin/dockeva ./code/dockeva.cpp
+#g++ -static -O3 -ffast-math -o ./bin/dockeva ./code/dockeva.cpp
+#g++ -o ./bin/dockeva ./code/dockeva.cpp
